2ed439a6dddd54ecbfac49e429518470d5eb85f0
Streakyfox7347 <foxy1998@gmail.com>
2025-05-09 22:21:03 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/scramble/', // Add this line here
   server: {
     host: "::",
     port: 8080,
